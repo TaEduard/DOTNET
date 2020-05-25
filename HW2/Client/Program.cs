29ca3b4a8f64a623ad823d352e84f5de10fceb83
Eduard Tamsa <eduard.ioan.tamsa@gmail.com>
2020-05-25 12:38:36 -0700

    Add files via upload

---
+++ HW2/Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    enum RequestType
+    {
+        ListDirectory,
+        GetFileContent
+    }
+
+    class Program
+    {
+        static void Connect(RequestType requestType, string path)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(new IPAddress(new byte[] { 127, 0, 0, 1 }), 12000);
+
+            var reader = new BinaryReader(client.GetStream());
+            var writer = new BinaryWriter(client.GetStream());
+            writer.Write((int)requestType);
+            writer.Write(path);
+            writer.Flush();
+            var lineCount = reader.ReadInt32();
+            for (int i = 0; i < lineCount; ++i)
+            {
+                var line = reader.ReadString();
+                Console.WriteLine(line);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int requestType = int.Parse(Console.ReadLine());
+            string path = Console.ReadLine();
+            try
+            {
+                Connect((RequestType)requestType, path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ HW2/Server/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    enum RequestType
+    {
+        ListDirectory,
+        GetFileContent
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            Console.WriteLine(ipAddress);
+            var listener = new TcpListener(ipAddress, 12000);
+            listener.Start();
+            while (listener.Server.IsBound)
+            {
+                var client = listener.AcceptTcpClient();
+                var thread = new Thread(new ParameterizedThreadStart(ClientHandler));
+                thread.Start(client);
+            }
+        }
+
+        static void HandleGetFileContent(BinaryReader reader, BinaryWriter writer)
+        {
+            var filePath = reader.ReadString();
+            try
+            {
+                var text = File.ReadAllLines(filePath);
+                writer.Write(text.Length);
+                foreach (var line in text)
+                {
+                    writer.Write(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                writer.Write(0);
+            }
+            finally
+            {
+                writer.Flush();
+            }
+        }
+
+        static void HandleListDirectory(BinaryReader reader, BinaryWriter writer)
+        {
+            var path = reader.ReadString();
+            try
+            {
+                var dirInfo = new DirectoryInfo(path);
+                var files = new List<string>();
+                foreach (var file in dirInfo.GetFiles())
+                {
+                    files.Add(file.FullName);
+                }
+                writer.Write(files.Count);
+
+                foreach (var file in files)
+                {
+                    writer.Write(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                writer.Write(0);
+            }
+            finally
+            {
+                writer.Flush();
+            }
+        }
+
+        static void ClientHandler(object clnt)
+        {
+            var client = (TcpClient)clnt;
+            var reader = new BinaryReader(client.GetStream());
+            var writer = new BinaryWriter(client.GetStream());
+            while (client.Connected)
+            {
+                try
+                {
+                    var request = (RequestType)reader.ReadInt32();
+                    switch (request)
+                    {
+                        case RequestType.GetFileContent:
+                            HandleGetFileContent(reader, writer);
+                            break;
+                        case RequestType.ListDirectory:
+                            HandleListDirectory(reader, writer);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}

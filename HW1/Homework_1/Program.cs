f10a5907c3b0948680d498176acb088b7ef8fe66
Eduard Tamsa <eduard.ioan.tamsa@gmail.com>
2020-05-25 12:56:00 -0700

    Add files via upload

---
+++ HW1/Homework_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace Lab1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://www.emag.ro/docking-stations/c?ref=search_menu_category";
+            HttpClient hClient = new HttpClient();
+            var html = hClient.GetStringAsync(url).Result;
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            var elemntsnameonpage = doc.DocumentNode.SelectNodes("//*[@class='card-item js-product-data']//a[@title]");
+            var elemntspriceonpage = doc.DocumentNode.SelectNodes("//*[@class='product-new-price']");
+            var elems= elemntsnameonpage.Zip(elemntspriceonpage, (name, price) => new { Name=name, Price=price });
+            try
+            {
+
+                foreach (var item in elems)
+                {
+                    string productname = item.Name.InnerHtml.ToString();
+                    string productprice = item.Price.InnerHtml.ToString();
+                    productname = productname.Replace("&quot;", "\"").Replace("&#039;","'");
+                    productprice = productprice.Replace("<sup>", ",").Replace("</sup>", "").Replace(" <span>Lei</span>","");
+                   Console.WriteLine(productname + " - " + productprice + " Lei");
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+           }
+    }
+}
